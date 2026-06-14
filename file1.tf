1f0403e995b8ec46e92466066e672d1abf424301
pramodholambe <pramodholambe5@gmail.com>
2026-06-14 17:49:52 +0530

    tag added

--- file1.tf
+++ file1.tf
@@ -4,10 +4,9 @@ provider "aws" {
 resource "aws_instance" "myec2"{
     ami= "ami-01a00762f46d584a1"
     instance_type= "t3.micro"
-tag= {
-        Name= "1st-ec2"
-        name= "pramod"
-        env= "dev"
+tag = {
+        Name = "1st-ec2"
+        name = "pramod"
+        env = "dev"
     }
-    
 }
