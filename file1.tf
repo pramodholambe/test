a14fa52fa1e73abff1a5c55ff9360d3aff7784b7
pramodholambe <pramodholambe5@gmail.com>
2026-06-14 19:13:31 +0530

    terraform block added

--- file1.tf
+++ file1.tf
@@ -1,7 +1,20 @@
+terraform {
+    backend "s3" {
+    bucket = "bucket-pramod"
+    region = "ap-south-1"
+    } 
+}
+
 provider "aws" {
     region= "ap-south-1"
 }
 resource "aws_instance" "myec2"{
     ami= "ami-01a00762f46d584a1"
     instance_type= "t3.micro"
+vpc_security_group_ids = ["sg-0207caf3f6f306804"]
+tags = {
+    Name = "2nd-ec2"
+    name = "pramod"
+    env = "dev"
+}
 }
